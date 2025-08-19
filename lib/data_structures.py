f4adde8f366e60208d48b33f6c715f31c60b2976
SALEH-abdulwahab <saleh200228@gmail.com>
2025-08-19 13:59:37 +0300

    cm

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,50 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
-
+    '''contains function get_names() that retrieves names from list of foods.'''
+    result = []
+    for food in spicy_foods:
+        result.append(food["name"])
+    return result
+# return [food["name"] for food in spicy_foods]   cleaner approach
 def get_spiciest_foods(spicy_foods):
-    pass
+    '''contains function get_spiciest_foods() that returns foods with a heat_level over 5.'''
+    result = []
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            result.append(food)
+    return result
+    # return [food for food in spicy_food if food["heat_level"] > 5] cleaner approach
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"]
+        # Explicit \n to match the test exactly
+        print(f"{name} ({cuisine}) | Heat Level: {'🌶' * heat_level}\n", end="")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            name = food["name"]
+            cuisine = food["cuisine"]
+            heat_level = food["heat_level"]
+            print(f"{name} ({cuisine}) | Heat Level: {'🌶' * heat_level}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total = 0
+    for food in spicy_foods:
+        total += food["heat_level"]
+    return total // len(spicy_foods)  # integer division since test expects 6, not 6.0
+
+    # return sum(food["heat_level"] for food in spicy_foods) // len(spicy_foods)   cleaner approach
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods 
